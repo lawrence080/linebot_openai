365603228beac0cbc6c49153dda155b2b5667c69
Lawrence <lawrencew711kb@gmail.com>
2023-06-19 18:06:23 +0800

    ai testing 3

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def callback():
 
 
 #處理訊息
-@handler.add(postback, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event, rep):
     if flag == True:
         msg = event.message.text
