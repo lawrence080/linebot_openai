ebdf06c6be7969363c30cbe466dc864f55ca4553
Lawrence <lawrencew711kb@gmail.com>
2023-06-19 16:05:09 +0800

    adding the selection testing 5

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def setUpInterface():
                 PostbackTemplateAction(
                     label='message',
                     display = "button testing ",
-                    data = 'richmenu'
+                    data = 'action=buy&itemid=1'
                     )
                 }
             )]
@@ -116,7 +116,7 @@ def setUpInterface():
 
 @handler.add(PostbackEvent)
 def buttontemplate(event):
-    if event.postback.data == 'richmenu':
+    if event.postback.data == 'action=buy&itemid=1':
         button_template_message = TemplateSendMessage(
         alt_text = "invisiable",
         template = ButtonsTemplate(
